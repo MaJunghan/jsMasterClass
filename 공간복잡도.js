7b5b29a17ab2da1e885c3c7327195a75f2e1ae3d
MaJunghan <wjdgksak@naver.com>
2022-10-07 23:23:57 +0900

    공간복잡도

---
+++ 공간복잡도.js
@@ -0,0 +1,5 @@
+// 입력이 커질수록 얼마나 많은 공간을 차지하는지 ? => 알고리즘 자체 메모리!
+
+// boolen, undefined, null 은 모두 불변 공간이다.
+// string은 O(n)만큼 공간을 차지함.  50자리면 1자리보다 50배를 더많이 차지함.
+// reference type(참조타입, 원시타입), array, object O(n)
