70350ac7cfc316a8dfd92824089bec3a1118d40a
MaJunghan <wjdgksak@naver.com>
2022-10-09 17:23:04 +0900

    object

---
+++ object.js
@@ -0,0 +1,16 @@
+// object
+
+// 정렬되지 않아도 잘 사용 할 수있음
+// 빠른접근 , 입력과 제거를 원할때 사용
+
+// insert O(1)
+// remove O(1)
+// search O(n)
+// access O(1)
+
+// Object method
+
+// Object.keys O(n)
+// Object.values O(n)
+// Object.entries O(n)
+// hasOwnProperty O(1) => 해당객체에 어떤값이 있는지 확인해서 true, false 반환
