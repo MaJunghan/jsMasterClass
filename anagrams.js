d98b8057a00584325f4108323ecb3af3647dc3a2
MaJunghan <wjdgksak@naver.com>
2023-01-24 17:46:55 +0900

    anagrams

--- anagrams.js
+++ anagrams.js
@@ -1,23 +1,20 @@
 function validAnagram(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
-  let counter1 = {};
-  let counter2 = {};
+  let count1 = {};
+  let count2 = {};
   for (let val of arr1) {
-    counter1[val] = (counter1[val] || 0) + 1;
+    count1[val] = (count1[val] | 0) + 1;
   }
   for (let val of arr2) {
-    counter2[val] = (counter2[val] || 0) + 1;
+    count2[val] = (count2[val] | 0) + 1;
   }
-  for (let key in counter1) {
-    if (!(key in counter2)) {
-      return false;
-    }
-    if (counter2[key] !== counter1[key]) {
-      return false;
-    }
+
+  for (let key in count1) {
+    if (!key in count2) return false;
+    if (count1[key] !== count2[key]) return false;
   }
+
   return true;
 }
-console.log(validAnagram("aaz", "zza")); // false
-console.log(validAnagram("qwerty", "qeywrt")); // true
+console.log(validAnagram("", "")); // true
 console.log(validAnagram("texttwisttime", "timetwisttext")); // true
--- 빈도수세기패턴.js
+++ 빈도수세기패턴.js
@@ -1,32 +1,13 @@
 function same(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    false;
-  }
-  let counter1 = {};
-  let counter2 = {};
-  for (let val of arr1) {
-    counter1[val] = (counter1[val] || 0) + 1;
-  }
-  for (let val of arr2) {
-    counter2[val] = (counter2[val] || 0) + 1;
-  }
-  console.log(counter1);
-  console.log(counter2);
-  for (let key in counter1) {
-    // 제곱값이 count2에 있는지?
-    if (!(key ** 2 in counter2)) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    let currentIndex = arr2.indexOf(arr1[i] ** 2);
+    if (currentIndex === -1) {
       return false;
     }
-    // 있는데 수량이 맞는지 ?
-    if (counter2[key ** 2] !== counter1[key]) {
-      return false;
-    }
+    console.log(arr2);
+    arr2.splice(currentIndex, 1);
   }
   return true;
 }
-
-console.log(same([1, 2, 3, 1], [4, 9, 1, 1]));
-
-// 빈도카운터란의 요점
-// 2개의 배열을 객체화 세분화하여 각 배열을 비교한다.
-// 향상된 속도 기대가능 O(n^) => O(n)
+same([1, 2, 3, 2], [9, 1, 4, 4]);
