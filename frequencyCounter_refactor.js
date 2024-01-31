2737c1c363a0b5c2371868c90327c43f37636f62
MaJungHan <wjdgksak@naver.com>
2024-01-31 16:14:30 +0900

    frequencyCount

---
+++ frequencyCounter_refactor.js
@@ -0,0 +1,25 @@
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  const frequencyCounter1 = {};
+  const frequencyCounter2 = {};
+
+  for (let x of arr1) {
+    frequencyCounter1[x] = frequencyCounter1[x] + 1 || 1;
+  }
+  for (let x of arr2) {
+    frequencyCounter2[x] = frequencyCounter2[x] + 1 || 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(same([1, 2, 3], [9, 1, 4]));
