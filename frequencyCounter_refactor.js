e7cca6657fdbe0a9350b4049c6068b55d65aafa4
MaJungHan <wjdgksak@naver.com>
2024-01-31 16:15:49 +0900

    in, of

--- frequencyCounter_refactor.js
+++ frequencyCounter_refactor.js
@@ -11,6 +11,7 @@ function same(arr1, arr2) {
   for (let x of arr2) {
     frequencyCounter2[x] = frequencyCounter2[x] + 1 || 1;
   }
+  // in은 객체 , of은 배열
   for (let key in frequencyCounter1) {
     if (!(key ** 2 in frequencyCounter2)) {
       return false;
@@ -23,3 +24,5 @@ function same(arr1, arr2) {
 }
 
 console.log(same([1, 2, 3], [9, 1, 4]));
+
+// n^2을 O(n)으로 개선
